63c6b856f0dbc05fb3b5adec413d9cc28bbd857c
masanori0117 <masanori.takahashi.0117@gmail.com>
2024-10-15 22:01:26 +0900

    modify the code of Taks1-4

--- task1-4.py
+++ task1-4.py
@@ -17,14 +17,14 @@ print(pi)
 # 2. 基本的な計算
 # 整数型の2つの変数を宣言してください。2つの変数を用いて、足す、引く、かける、割る、割った余りを出力してください。
 
-a = 5
-b = 2
+num1 = 5
+num2 = 2
 
-addition = a + b
-subtraction = a - b
-multiplication = a * b
-divion = a / b
-modulo = a % b
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+divion = num1 / num2
+modulo = num1 % num2
 
 print(addition)
 print(subtraction)
@@ -36,11 +36,11 @@ print(modulo)
 # 3. 条件式と論理型（boolean）について
 # 初期値がFalseである論理型の変数を宣言してください。問題2で宣言した2つの変数を足した結果が偶数であれば、論理型の変数にTrueを代入してください。
 
-x = False
+is_even = False
 
 if addition %2 == 0:
-	x = True
-print(x)
+    is_even = True
+print(is_even)
 
 
 # 4. 条件式
@@ -49,7 +49,7 @@ print(x)
 # 偶数なら「偶数です」
 # 奇数なら「奇数です」
 
-if x == True:
-	print("偶数です")
+if is_even == True:
+    print("偶数です")
 else:
-	print("奇数です")
+    print("奇数です")
