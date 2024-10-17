5acbc593fcaaabf3b87bf30dbdb8919132df41c2
masanori0117 <masanori.takahashi.0117@gmail.com>
2024-10-18 01:42:41 +0900

    modified task5-8

--- task5-8.py
+++ task5-8.py
@@ -1,6 +1,5 @@
 ### 5. for文の基礎
 # 1~10までの数字をfor文を使って出力してください
-
 for number in range(1, 11):
     print(number)
 
@@ -8,7 +7,6 @@ for number in range(1, 11):
 # 35 ~ 46までの数字をfor文を使って出力してください
 for number in range(35, 47):
     print(number)
-
 
 ### 7.for文と条件式の組み合わせ
 # 40 ~ 50までの数字の中で奇数の数字のみを出力してください
