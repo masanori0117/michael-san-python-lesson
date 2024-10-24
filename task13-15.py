9481e1c75f395dc2da12c32a7292a50b832dab3f
masanori0117 <masanori.takahashi.0117@gmail.com>
2024-10-25 03:20:02 +0900

    added task13-15

---
+++ task13-15.py
@@ -0,0 +1,39 @@
+### 13, 図形の表示
+print("### 13, 図形の表示")
+print("0")
+print("00")
+print("000")
+print("この図形をfor文を使って出力してください。")
+print("")
+
+for number in range(1,4):
+    print("0" * number)
+
+print("")
+### 14, 図形の表示
+print("### 14, 図形の表示")
+print("0")
+print("000")
+print("00000")
+print("この図形をfor文を使って出力してください。")
+print("")
+
+for number in range(1,6,2):
+    print("0" * number)
+
+print("")
+### 15, 図形の表示
+print("### 15, 図形の表示")
+print("0")
+print("000")
+print("00000")
+print("000")
+print("0")
+print("この図形をfor文を使って出力してください。")
+print("")
+
+for number in range(1,6,2):
+    print("0" * number)
+for number in range(3,0,-2):
+    print("0" * number)
+
